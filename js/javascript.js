c38494936d25361c718bec071743dd6c4b557820
abbyythompson <abbyythompson@gmail.com>
2018-12-10 19:01:08 -0800

    cleaned up the simplicity, creating divs for thumbnails of work

--- js/javascript.js
+++ js/javascript.js
@@ -2,28 +2,17 @@ $(document).ready(function() {
 
   $('#descriptionBody').fadeIn(1000);
   $('#contactBody').css('display', 'none');
-  $('#workBody').css('display', 'none');
+
+  $('#workEx1').css('display', 'none');
+  $('#workEx2').css('display', 'none');
+
 
   //Click Me
   $('#description').click(function() {
     event.preventDefault();
     $('#contactBody').hide();
-    $('#workBody').hide();
     $('#descriptionBody').fadeIn(1000);
 
-    $(this).addClass('active');
-    $('#work').removeClass('active');
-    $('#contact').removeClass('active');
-  });
-
-  //Click Work
-  $('#work').click(function() {
-    event.preventDefault();
-    $('#contactBody').hide();
-    $('#descriptionBody').hide();
-    $('#workBody').fadeIn(1000);
-
-    $('#description').removeClass('active');
     $(this).addClass('active');
     $('#contact').removeClass('active');
   });
@@ -32,11 +21,9 @@ $(document).ready(function() {
   $('#contact').click(function() {
     event.preventDefault();
     $('#descriptionBody').hide();
-    $('#workBody').hide();
     $('#contactBody').fadeIn(1000);
 
     $('#description').removeClass('active');
-    $('#work').removeClass('active');
     $(this).addClass('active');
   });
 
