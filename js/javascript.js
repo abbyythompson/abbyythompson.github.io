fb09dcc6ba0bb877b145207bcd164efa9b428661
abbyythompson <abbyythompson@gmail.com>
2019-02-27 12:29:22 -0800

    work content creation

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,10 @@
 $(document).ready(function() {
-
-  // $('#workBody').fadeIn(1000);
-  // $('#contactBody').fadeIn(1000);
-
   $('div.hidden').fadeIn(1000).removeClass('hidden');
 
-  //
-  //Click Work
-  // $('#description').click(function() {
-  //   event.preventDefault();
-  //
-  //   $('#contactBody').hide();
-  //   $('#workBody').fadeIn(1000);
-  //
-  //   $(this).addClass('active');
-  //   $('#contact').removeClass('active');
-  // });
-  //
-  // //Click Contact
-  // $('#contact').click(function() {
-  //   event.preventDefault();
-  //
-  //   $('#workBody').hide();
-  //   $('#contactBody').fadeIn(1000);
-  //
-  //   $('#description').removeClass('active');
-  //   $(this).addClass('active');
-  // });
+  $("GLMIexplanation").hide();
+
+  $("#moreGLMI").click(function(){
+    $("#GLMIexplanation").fadeIn(1000);
+    $("#moreGLMI").hide();
+  });
 });
